b01f8b2739ab9d406bb0edaebeea0536fd546d67
Zana H <zhertica@gmail.com>
2020-05-21 21:07:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,10 +59,18 @@ def monopoly_with_fourth_tier
         four_pieces_owned:200
       },
       :names=> {
-        :reading_railroad=> {},
-        :pennsylvania_railroad=> {},
-        :b_and_o_railroad=> {},
-        :shortline_railroad=> {}
+        :reading_railroad=> {
+          mortgage_value: 100
+        },
+        :pennsylvania_railroad=> {
+          mortgage_value: 200
+        },
+        :b_and_o_railroad=> {
+          mortgage_value: 400
+        },
+        :shortline_railroad=> {
+          mortgage_value: 800
+        }
       }
     }
   }
